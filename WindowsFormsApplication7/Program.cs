ee9d126ed22f3ceaa70d2cacf30bac9d368fa3ce
yeongil-PC\yeongil <ecolo9ger@gmail.com>
2017-04-13 23:10:36 +0900

    폼 만들기

--- WindowsFormsApplication7/Program.cs
+++ WindowsFormsApplication7/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApplication7
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new NaverLogin());
+            Application.Run(new toNode());
         }
     }
 }
