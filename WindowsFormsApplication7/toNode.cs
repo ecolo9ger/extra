842ddf32371d1070dbdb98a713748a532f8d4da3
yeongil-PC\yeongil <ecolo9ger@gmail.com>
2017-04-13 23:30:26 +0900

    버튼 수정

--- WindowsFormsApplication7/toNode.cs
+++ WindowsFormsApplication7/toNode.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication7
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("저장됨");
+        }
     }
 }
